28359235faf28c985c9d301187567032c419be37
Rama Tri Agung <ramatriagung91@gmail.com>
2020-07-26 14:26:30 +0700

    broadcast receiver

---
+++ MyBroadcastReceiver/app/src/main/java/com/example/mybroadcastreceiver/DownloadService.kt
@@ -0,0 +1,28 @@
+package com.example.mybroadcastreceiver
+
+import android.app.IntentService
+import android.content.Intent
+import android.util.Log
+import java.lang.Exception
+
+class DownloadService : IntentService("DownloadService") {
+
+    companion object{
+        val TAG = DownloadService::class.java.simpleName
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        Log.d(TAG, "Download service dijalankan")
+
+        if (intent != null){
+            try {
+                Thread.sleep(5000)
+            } catch (e: InterruptedException){
+                e.printStackTrace()
+            }
+
+            val notifyFinishIntent = Intent(MainActivity.ACTION_DOWNLOAD_STATUS)
+            sendBroadcast(notifyFinishIntent)
+        }
+    }
+}
